e26b023263b467a600bf0cdfdba38bfc54b2e561
noor <aniiahmed380@gmail.com>
2024-05-09 13:01:38 +0500

    add

---
+++ index.ts
@@ -0,0 +1,91 @@
+import inquirer from "inquirer";
+// todo arry 
+// function
+// operation
+
+
+///todo array
+
+let todos: string[] = ['bilal', 'noor'];
+
+//function 
+
+async function createTodo(todos: string[]) {
+
+    do {
+        let ans = await inquirer.prompt({
+            type: "list",
+            message: "select your operation",
+            name: "select",
+            choices: ['add', 'update', 'view', 'delete']
+
+        })
+
+
+
+        /// operation
+        ///add 
+
+        if (ans.select == 'add') {
+            let addTodo = await inquirer.prompt({
+                type: 'input',
+                message: 'Add item',
+                name: 'todo'
+            })
+            todos.push(addTodo.todo);
+            let newTodo = todos.filter(val => val !== addTodo.todo);
+            todos = [...newTodo, addTodo.todo]
+            console.log(todos);
+
+        }
+
+        // update
+
+        if (ans.select == 'update') {
+            let updateTodo = await inquirer.prompt({
+                type: 'list',
+                message: 'select the item to update',
+                choices: todos.map(item => item),
+                name: 'todo'
+            })
+
+            let addTodo = await inquirer.prompt({
+                type: 'input',
+                message: 'Add item',
+                name: 'todo'
+            })
+            let newTodo = todos.filter(val => val !== updateTodo.todo);
+            todos = [...newTodo, addTodo.todo]
+            console.log(todos);
+
+
+        }
+        if (ans.select == 'view') {
+            console.log(todos);
+
+
+        }
+        if (ans.select == 'delete') {
+            let deleteTodo = await inquirer.prompt({
+                type: 'list',
+                message: 'select the item to update',
+                choices: todos.map(item => item),
+                name: 'todo'
+            })
+            let newTodo = todos.filter(val => val !== deleteTodo.todo);
+            todos = [...newTodo]
+            console.log(todos);
+
+
+        }
+
+    } while (true)
+
+}
+
+
+
+createTodo(todos);
+
+/// operation 
+
